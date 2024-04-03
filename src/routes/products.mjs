9f001667c720acbb3d74751cf1e73254a56dc046
SIVA <siva742002s@gmail.com>
2024-04-03 13:25:48 +0530

    server checks on signed cookies and reply response if client had valid cookie then valid response else error message And this module learned the cookies and signed cookies

--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,7 +3,16 @@ import { Router } from "express";
 const router = Router();
 
 router.get( '/api/products', ( request, response ) => {
-    response.send( [ { id: 123, name: 'chicken Breast', price: 12.99 } ] );
+    // console.log( request.headers.cookie );
+    // console.log( request.cookies );
+    // console.log( request.signedCookies );
+
+    if ( request.signedCookies.Hello && request.signedCookies.Hello === "World" ) {
+
+        return response.send( [ { id: 123, name: 'chicken Breast', price: 12.99 } ] );
+    }
+    return response.status( 403 )
+        .send( { msg: "Sorry you need correct cookies!" } );
 } );
 
 

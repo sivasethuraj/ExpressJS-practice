a77b3a40332caae89dd6ee7230008ff6bdeaad1e
SIVA <siva742002s@gmail.com>
2024-04-03 13:25:36 +0530

    whenever user visit the base url as '/' server creates the signed cookie in their client machine

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,16 @@
 import express from 'express';
-
+import cookieParser from 'cookie-parser';
 import routes from './routes/index.mjs';
 
 const app = express();
 app.use( express.json() );
+app.use( cookieParser( "sivasecret" ) );
 app.use( routes );
 
 const PORT = process.env.PORT || 3000;
 
 app.get( '/', ( request, response ) => {
+    response.cookie( "Hello", "World", { maxAge: 30000, signed: true } );
     response.status( 201 ).send( { msg: 'Hello!' } );
 } );
 
